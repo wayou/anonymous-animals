bee4f38e2a52299ba8bfbbe96f1d3f02777ec682
wayou <liuwayong@gmail.com>
2017-03-27 01:41:51 +0800

    add title for page

--- download_animals.py
+++ download_animals.py
@@ -40,7 +40,7 @@ html_wrapper = """
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title></title>
+        <title>anonymous animals from google docs</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <style>
